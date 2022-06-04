7ffc75fae532ee7b9359651db6669672a6efe8d2
mayurthakar11 <93188001+mayurthakar11@users.noreply.github.com>
2022-06-04 19:14:53 +0530

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,106 @@
+//back to top
+
+/* Back to Top Scroll S */
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 80) {
+        $('#back-top').show();
+    } else {
+        $('#back-top').hide();
+    }
+});
+$('#back-top').click(function() {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 2500);
+    return false;
+});
+/* Back to Top Scroll E */
+
+
+
+//scroll header
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() >= 1) {
+        $('header').addClass('fixed animated fadeInDown slow');
+    } else {
+        $('header').removeClass('fixed animated fadeInDown slow');
+    }
+});
+
+/* Bootstrap Select S */
+
+$(window).on("load resize", function() {
+    var owl = $('.info-slider-main.owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        loop: false,
+        nav: true,
+        dots: false,
+        lazyLoad: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        autoHeight: true,
+        mouseDrag: false,
+        navText: ["<i class='icon-left-arrow' title='Previous'></i>", "<i class='icon-right-arrow' title='Next'></i>"],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            480: {
+                items: 2,
+                margin: 15,
+            },
+            1024: {
+                items: 3,
+            },
+            1200: {
+                items: 3,
+            }
+        }
+    });
+});
+
+// calender S
+moment.locale("en", {
+    week: { dow: 1 } // Monday is the first day of the week
+});
+
+$("#datetimepicker12").datetimepicker({
+    inline: true,
+    sideBySide: true,
+    format: "DD-MM-YY",
+    stepping: 30,
+    minDate: moment()
+});
+// calender E
+
+
+// video auto play S
+// $(window).on("load", function() {
+//     var deviceAgent = navigator.userAgent.toLowerCase();
+//     var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+//     var vid = document.getElementById("video");
+//     var isAndroid = /(android)/i.test(navigator.userAgent);
+//     if (agentID) {
+//         window.setInterval(function() {
+//             if (vid.paused) {
+//                 //alert("paused");
+//                 $('#rotate-video').removeClass('pause');
+//             }
+//         }, 100);
+//         $('#rotate-video').on("click", function() {
+//             if ($(this).hasClass("pause")) {
+//                 $(this).removeClass('pause');
+//                 vid.pause();
+//             } else {
+//                 $(this).addClass('pause');
+//                 vid.play();
+//             }
+//         });
+//     }
+//     if (isAndroid) {
+//         $('#rotate-video').addClass('noAndroid');
+//     }
+// });
+// video auto play E
---
+++ template/header.php
@@ -0,0 +1,157 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0" />
+    <title>
+        TCI Airport
+    </title>
+    <!-- Favicon Icon S -->
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/apple-touch-icon-144.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/images/apple-touch-icon-57.png" />
+    <!-- Favicon Icon E -->
+    <!-- font link -->
+ 
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- mobile menu -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-select.css">
+    <link rel="stylesheet" href="assets/css/datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/mobile-menu.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="assets/css/jquery.fancybox.css">
+
+     
+    <!-- custom css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animation.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/page-style.css"> -->
+    <script src="assets/js/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id="buorg" class="buorg">
+        <div class="buorg__text">For a better view on Turks & Caicos Islands Airports Authority, <a href="https://support.microsoft.com/en-us/help/17621/internet-explorer-downloads" title="Update Your Browser" target="_blank">Update Your Browser.</a></div>
+    </div>
+
+    <div id="back-top" title="Scroll To Top" style="display: none;">
+        <i class="icon-plane"></i>
+    </div>
+    <div class="wrapper" id="wrapper">
+        <header class="main-header" data-aos="fade-down">
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="header-top">
+                            <div class="top-left">
+                            <ul class="social-link">
+                                <li><a href=""><i class="icon-facebook"></i></a></li>
+                                <li><a href=""><i class="icon-linkedin"></i></a></li>
+                                <li><a href=""><i class="icon-twitter"></i></a></li>
+                                <li><a href=""><i class="icon-youtube"></i></a></li>
+                            </ul>
+                                <a href="tel:6499464420" class="phone-num"><i class="icon-phone"></i>649 946 4420</a>
+                            </div>
+                            <div class="top-right">
+                                <div class="right-part">
+                                     <i class="icon-clock1"></i>
+                                    <span class="clock-time">7:10 PM</span>
+                                </div>
+                                <div class="right-part">
+                                    <img src="assets/images/sunny.png" alt="">
+                                    <span class="wether-text">79<sb>F</sb></span>
+                                </div>
+                                <div class="right-part">
+                                <a class="search-btn" href="javascript:void(0)" title="" onclick="openSearch()"><i class="icon-search"></i></a>
+                                    <div id="searchOverlay" class="search-overlay">
+                                    <div class="search-content">
+                                        <span class="closebtn" onclick="closeSearch()" title="close"><i class="icon-close"></i></span>
+                                        <form action="">
+                                            <div class="search_from form-group">
+                                                <input type="text" class="form-control">
+                                                <button type="submit"><i class="icon-search"></i></button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="right-part">
+                                <div class="form-group select-group">
+                                        <select class="selectpicker form-control">
+                                            <option value=""> EN</option>
+                                            <option value="">USA</option>
+                                         </select>
+                                     </div>
+                                </div>
+                                <div class="right-part">
+                                <div class="form-group select-group">
+                                        <select class="selectpicker form-control">
+                                            <option value="">Airports</option>
+                                            <option value="">Airports</option>
+                                         </select>
+                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="header-section">
+                            <div class="logo">
+                                <a href="" title="Turks & Caicos Islands Airports Authority"><img src="assets/images/logo.png" alt="Turks & Caicos Islands Airports Authority"></a>
+                            </div>
+                            <div class="menu mobile-menu">
+                                <div class="menu_mobile_visibility">
+                                    <a href="" title="Turks & Caicos Islands Airports Authority"><img src="assets/images/logo.png" alt="Turks & Caicos Islands Airports Authority"></a>
+                                </div>
+                                <ul>
+                                <li><a href="javascript:void(0);" title="Home" class="active">Home</a></li>
+                                <li><a href="javascript:void(0);" title="About">About</a></li>
+                                <li><a href="javascript:void(0);" title="Airports">Airports</a></li>
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" title="AIS">AIS</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="javascript:void(0);" title="AIS">AIS</a></li>
+                                        <li><a href="javascript:void(0);" title="AIS">AIS</a></li>
+                                        <li><a href="javascript:void(0);" title="AIS">AIS</a></li>
+                                    </ul>
+                                </li> 
+                                <li>
+                                        <a href="" title="Publications">Publications</a>
+                                    </li>
+                                    <li>
+                                        <a href="" title="Sustainability">Sustainability</a>
+                                    </li>
+                                    <li>
+                                        <a href="" title="Join Us">Join Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="" title="Media">Media</a>
+                                    </li>
+                                    <li>
+                                        <a href="" title="Complaints">Complaints</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="header-flightbtn">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Find Flight">
+                                <button type="submit">Book Flight</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="javascript:void(0)" id="nav-mobile-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+            </div>
+        </header>
+        <!--<header>-->
